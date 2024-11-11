78a5ac96605e77bdf04187839036e2d01f6c332a
Lidia483 <lidia.kocon@wp.pl>
2024-11-11 19:02:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include "theil_sen.h"
 
 int main() {
-    float x[] = {1, 2, 3, 4, 5};
-    float y[] = {2, 4, 5, 4, 5};
+    float x[] = {2, 5, 10, 15, 20};
+    float y[] = { 0.4, 0.8, 1, 2, 3};
     size_t length = sizeof(x) / sizeof(x[0]);
 
     TheilSenResult result = TheilSen_Estimate(x, y, length);
@@ -13,3 +13,5 @@ int main() {
 
     return 0;
 }
+ //Nachylenie: 0.138889
+//Wyraz wolny: 0.105556
