59fae64bdb6b301aa7191f3a19890ba54fd9fee9
Lidia483 <lidia.kocon@wp.pl>
2024-12-10 01:37:04 +0100

    Update theil_sen.h

--- theil_sen.h
+++ theil_sen.h
@@ -1,13 +1,15 @@
 #ifndef THEIL_SEN_H
 #define THEIL_SEN_H
 
-#include <stddef.h>
+#include <stddef.h> // Potrzebne dla typu size_t
 
+// Struktura przechowująca wynik estymacji
 typedef struct {
-    float slope;
-    float intercept;
-} TheilSenResult;
+    float nachylenie;    // Nachylenie
+    float wyraz_wolny;   // Wyraz wolny
+} WynikTheilSena;
 
-TheilSenResult TheilSen_Estimate(const float* x, const float* y, size_t length);
+// Funkcja obliczająca estymator Theil-Sen
+WynikTheilSena TheilSen_Estymator(const float* x, const float* y, size_t dlugosc, uint32_t *rzad);
 
 #endif // THEIL_SEN_H
